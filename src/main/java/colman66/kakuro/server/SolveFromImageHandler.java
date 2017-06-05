2d0253f1ef0355310357b37b398c565ba41c87bc
Moti Zilberman <motizilberman@Motis-MacBook-Pro.local>
2017-06-05 21:33:11 +0100

    getBoardFromImage: Call python script via env vars

--- src/main/java/colman66/kakuro/server/SolveFromImageHandler.java
+++ src/main/java/colman66/kakuro/server/SolveFromImageHandler.java
@@ -51,18 +51,6 @@ public class SolveFromImageHandler implements Route {
 
   private JsonNode getBoardFromImage(FileItem image) throws JsonProcessingException, IOException
   {
-    // TODO: call python code
-    String dummyBoardJson =
-      "[[\"X\", {\"down\":3}, {\"down\":4}, \"X\", \"X\", \"X\", \"X\", {\"down\":15}, {\"down\":3}]," +
-      "[{\"right\":4}, null, null, {\"down\":16}, {\"down\":6}, \"X\", {\"right\":3}, null, null]," +
-      "[{\"right\":10}, null, null, null, null, {\"down\":14}, {\"down\":16, \"right\":7}, null, null]," +
-      "[\"X\", \"X\", {\"down\":21, \"right\":16}, null, null, null, null, null, \"X\"]," +
-      "[\"X\", {\"right\":3}, null, null, {\"down\":3, \"right\":11}, null, null, null, \"X\"]," +
-      "[\"X\",{\"right\":6}, null, null, null, {\"down\":4, \"right\":10}, null, null, \"X\"]," +
-      "[\"X\", {\"down\":4, \"right\":19}, null, null, null, null, null, {\"down\":3}, {\"down\":4}]," +
-      "[{\"right\":6}, null, null, \"X\", {\"right\":10}, null, null, null, null]," +
-      "[{\"right\":7}, null, null, \"X\", \"X\", \"X\", {\"right\":4}, null, null]]";
-
-    return mapper.readTree(dummyBoardJson);
+    return ImageProcessing.getBoardFromImage(image);
   }
 }
